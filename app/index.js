b601317127add45dbd62c2382bb5d861a995facf
Rut <rsantos@intellisys.com.do>
2020-02-05 16:47:28 -0400

    First commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Popular from './components/Popular'
+import Battle from './components/Battle'
+import "./index.css";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className='container'>
+        <Battle />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
